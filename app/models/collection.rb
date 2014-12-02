69a89d01e8bde6297a90af0743d1fa2a712c2277
Joanne <joannexu.gm@gmail.com>
2014-12-02 02:11:15 -0800

    validations

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,4 @@
 class Collection < ActiveRecord::Base
   belongs_to :user
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :collections
   has_many :posts
+  validates_associated :collections
+  validates_associated :posts
+  validates :name, presence: true, uniqueness: true
 end
