ac6bcde52d23de6f2b272d05faa35377bbbd8ca7
Joanne <joannexu.gm@gmail.com>
2014-12-04 15:59:08 -0800

    commiting everything

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -4,6 +4,16 @@ class CollectionsController < ApplicationController
 		@collection = @user.collections.build
 	end
 
+	def create
+		@collection = Collection.new collection_params
+		if @collection.save
+			flash[:success] = 'Created!'
+			redirect_to @collection.user
+		else
+			render 'new'
+		end
+	end
+
 	def show
 		@collection = Collection.find params[:collection_id]
 		@collections = @collection.posts.order(:created_at).page(params[:page]).per(2)
@@ -15,4 +25,25 @@ class CollectionsController < ApplicationController
 		@collection = @user.collections
 		@post = Post.find params[:post_id]
 	end
+
+	def edit
+		@collection = Collection.find params[:id]
+	end
+
+	def update
+		@collection = Collection.find params[:id]
+		if @collection.update collection_params
+			flash[:success] = 'Updated!'
+			redirect_to @collection.user
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+	def collection_params
+		params.require(:collection).permit(:user_id, :name)
+	end
+
 end
